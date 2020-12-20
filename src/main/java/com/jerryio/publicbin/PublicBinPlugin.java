2c7cad5487e8e3792a0cdcf549274ab024ea08f8
Jerry Lum <serverofjerry@gmail.com>
2020-12-20 13:31:58 +0800

    initial project

---
+++ src/main/java/com/jerryio/publicbin/PublicBinPlugin.java
@@ -0,0 +1,17 @@
+package com.jerryio.publicbin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PublicBinPlugin extends JavaPlugin {
+	
+    @Override
+    public void onEnable() {
+        
+    }
+
+    
+    @Override
+    public void onDisable() {
+
+    }
+}
